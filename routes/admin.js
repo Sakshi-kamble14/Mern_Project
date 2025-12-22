f7816b7f1a28c4b34dbed86b8d6252f885a3998e
Sakshi Kamble <sakshikamble300@gmail.com>
2025-12-22 09:07:38 +0530

    Some changes done

--- routes/admin.js
+++ routes/admin.js
@@ -5,52 +5,56 @@ const result=require('../utils/result')
 const router=express.Router()
 
 
-router.get("/all-courses", (req, res) => {
-    const { startDate, endDate } = req.query
+router.get("/all-courses", (request, response) => {
+    const { startDate, endDate } = request.query
 
-    const sql=`Select * from courses WHERE start_date >= CURRENT_DATE()  AND end_date <= CURRENT_DATE()`
 
+    const sql=`Select * from courses`
+    
+    
     pool.query(sql, [startDate,endDate], (error, data) => {
-        res.send(result.createResult(error, data))
+        console.log(data)
+        response.send(result.createResult(error, data))
     })
 })
 
 router.get('/all-active-course',(request,response)=>{
- const sql = `SELECT * FROM courses WHERE start_date <= CURRENT_DATE() AND end_date >= CURRENT_DATE()`;    pool.query(sql,(error,data)=>{
+ const sql = `SELECT * FROM courses WHERE start_date <= CURRENT_DATE() AND end_date >= CURRENT_DATE()`;    
+ pool.query(sql,(error,data)=>{
         response.send(result.createResult(error,data))
     })
 })
 
-router.post("/add-course", (req, res) => {
-    const {courseName,description,fees,startDate,endDate,videoExpireDays} = req.body
+router.post("/add-course", (request, response) => {
+    const {courseName,description,fees,startDate,endDate,videoExpireDays} = request.body
 
     const sql = `INSERT INTO courses (course_name, description, fees, start_date, end_date, video_expire_days) VALUES (?,?,?,?,?,?) `
 
     pool.query(sql,[courseName, description, fees, startDate, endDate, videoExpireDays],(error, data) => {
-            res.send(result.createResult(error, data))
+            response.send(result.createResult(error, data))
         }
     )
 })
 
-router.put("/update/:courseId", (req, res) => {
-    const { courseId } = req.params
-    const {courseName,description,fees,startDate,endDate,videoExpireDays} = req.body
+router.put("/update/:courseId", (request, response) => {
+    const { courseId } = request.params
+    const {courseName,description,fees,startDate,endDate,videoExpireDays} = request.body
 
     const sql = `UPDATE courses SET course_name=?,description=?,fees=?,start_date=?,end_date=?,video_expire_days=? WHERE course_id =?`
 
     pool.query(sql,[courseName,description,fees,startDate,endDate,videoExpireDays,courseId],(error, data) => {
-            res.send(result.createResult(error, data))
+            response.send(result.createResult(error, data))
         }
     )
 })
 
-router.delete("/delete/:courseId", (req, res) => {
-    const { courseId } = req.params
+router.delete("/delete/:courseId", (request, response) => {
+    const { courseId } = request.params
 
     const sql = `DELETE FROM courses WHERE course_id=?`
 
     pool.query(sql, [courseId], (error, data) => {
-        res.send(result.createResult(error, data))
+        response.send(result.createResult(error, data))
     })
 })
 
