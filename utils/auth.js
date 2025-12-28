0b29e75f1a7384e40602c96d48f901de33ad39cd
Atul1x <76177181+Atul1x@users.noreply.github.com>
2025-12-28 20:19:50 +0530

    Refactored authentication middleware and functions

--- utils/auth.js
+++ utils/auth.js
@@ -1,28 +1,43 @@
-const jwt = require("jsonwebtoken")
+const jwt = require('jsonwebtoken')
 
-const config = require("./config")
-const result = require("./result")
+const config = require('./config')
+const result = require('./result')
 
+const allowedUrls = ['/user/login', '/user/register',
+                    '/student/my_course_with_videos', '/student/my-courses',
+                    '/course/all-courses', '/course/all-active-courses',
+                    '/video/all-videos'
+];
 
-function authUser(req,res,next){
-    const allAllowedUrls = ["/user/login" , "/user/register"]
-    if(allAllowedUrls.includes(req.url))
-        next()
-    else{
-        const token = req.headers.token
-        if(!token)
-            res.send(result.createResult("Token is missing"))
-        else{
-            try{
-                const payload = jwt.verify(token,config.SECRET)
-                // req.header.payload = payload
-                req.headers.email = payload.email
-              return next()
-            }catch(ex){
-               return res.send(result.createResult("Token is Invalid"))
-            }
-        }
+function authenticateToken(request, response, next) {
+    // for ever incoming request this middleware will be called
+    const allAllowedUrls = allowedUrls;
+
+    if (allAllowedUrls.includes(request.path)) {
+        return next(); // Skip authentication for allowed URLs
+    }
+    else {
+        return verifyToken(request, response, next);
     }
 }
 
-module.exports={authUser}
+function verifyToken(request, response, next) {
+    const token = req.headers.token;
+    if (token == null) {
+        return response.status(401).send(result.createResult("Token not found"));
+    }
+    else {
+        const Payload = jwt.verify(token, config.JWT_SECRET)
+        req.headers.email = Payload.email
+        next();
+    }
+}
+
+function authAdmin(req, res, next) {
+    if (req.headers.role == "admin")
+        next()
+    else
+        res.send(result.createResult("You are not authorized"))
+}
+
+exports = { authenticateToken, authAdmin }
