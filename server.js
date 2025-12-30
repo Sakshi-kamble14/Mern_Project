a29950c5bafee7dd3090963c34ab3e8f172cbb9e
Sakshi Kamble <sakshikamble300@gmail.com>
2025-12-30 23:10:56 +0530

    Protecting routes

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
-const express = require('express')
+const express=require('express')
+const cors=require('cors')
 
-const app = express()
-const PORT = 7400
-
-const courseRouter = require('./routes/course')
+const app=express()
+const PORT=7400
+const courseRouter=require('./routes/course')
 const studRouter = require("./routes/student")
 const videoRouter = require("./routes/video")
+const userRouter=require("./routes/user")
+const { authenticateToken } = require('./utils/auth')
 
-const userRouter = require("./routes/user")
-const auth = require('./utils/auth')
-
-//middleware to parse json data
+app.use(cors())
 app.use(express.json())
+app.use(authenticateToken)
+app.use('/course',courseRouter)
+app.use("/student",studRouter)
+app.use("/video",videoRouter)
+app.use("/user",userRouter)
 
-//routes (authentication NOT needed for login/register)
-app.use("/user", userRouter)
-
-// Apply authentication middleware ONLY to protected routes
-
-app.use(auth.authenticateToken)
-app.use(auth.authAdmin)
-app.use('/course', courseRouter)
-app.use("/student", studRouter)
-app.use("/video", videoRouter)
-
-app.listen(PORT, 'localhost', () => {
-    console.log("Server started at port ", PORT)
+app.listen(PORT,'localhost',()=>{
+    console.log("Server started at port ",PORT)
 })
